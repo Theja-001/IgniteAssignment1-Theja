f047f6eca678b655c636df0bf5738811321420e9
Theja Anna <78838984+Theja-001@users.noreply.github.com>
2021-03-19 11:32:19 +0530

    Update and rename Question2 to Question2.java

---
+++ Question2.java
@@ -0,0 +1,30 @@
+//BubbleSort
+package com.company;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		int[]b_nums= {36,19,29,12,5};
+		int temp;
+		for(int i=0;i<a.length;i++)
+		{
+			int flag=0;
+			for(int j=0;j<a.length-1;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+			
+		}
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+
+	}
+}
+
